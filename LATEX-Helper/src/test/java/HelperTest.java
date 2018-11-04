6e428c140dd4b1113f9cc9346644d8f841edc538
alekmus <alekmus@users.noreply.github.com>
2018-11-04 18:59:09 +0200

    maven eteenpäin

---
+++ LATEX-Helper/src/test/java/HelperTest.java
@@ -0,0 +1,36 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Aleksi
+ */
+public class HelperTest {
+    
+    public HelperTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    
+    @Test
+    public void hello() {}
+}
